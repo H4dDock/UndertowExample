edb5375f1701fcabf6e3ecf51a6a0627d98a1c84
H4dDock <maxolo.di@gmail.com>
2019-03-12 21:50:04 +0300

    Added new handler to get data from client, for show this data on html page (create form) added freemarker.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,52 @@
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
 import io.undertow.Undertow;
+import io.undertow.server.handlers.form.EagerFormParsingHandler;
+import io.undertow.server.handlers.form.FormData;
+import io.undertow.server.handlers.form.FormDataParser;
+import io.undertow.server.handlers.resource.ClassPathResourceManager;
+import io.undertow.server.handlers.resource.ResourceHandler;
 import io.undertow.util.Headers;
 import io.undertow.Handlers;
 
-import java.util.ArrayDeque;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
+        cfg.setClassLoaderForTemplateLoading(Main.class.getClassLoader(),"template");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+
+        final Template nameTemplate = cfg.getTemplate("name.html");
+
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
                 .setHandler(Handlers.path()
                         .addExactPath("helloworld",(exchange)->{
-                            String name = exchange.getQueryParameters().getOrDefault("name", new ArrayDeque<String>()).poll();
                          exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                         exchange.getResponseSender().send("Hello World by" + name);
+                         exchange.getResponseSender().send("Hello World");
                         })
-                        .addPrefixPath("/", exchange -> {
-                            String name = exchange.getQueryParameters().getOrDefault("name", new ArrayDeque<String>()).poll();
-                            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                            exchange.getResponseSender().send("Hello, " + name);
-                        })
+                        .addExactPath("form",new EagerFormParsingHandler().setNext((exchange)->{
+                            FormData form = exchange.getAttachment(FormDataParser.FORM_DATA);
+                            FormData.FormValue firstFv = form.getFirst("name");
+                            String name = firstFv.getValue();
+
+                            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
+
+                            HashMap<String, Object> templateData = new HashMap<String, Object>();
+                            templateData.put("name", name);
+                            StringWriter stringWriter = new StringWriter();
+                            nameTemplate.process(templateData, stringWriter);
+
+                            exchange.getResponseSender().send(stringWriter.toString());
+                        }))
+                        .addPrefixPath("/",new ResourceHandler(new ClassPathResourceManager(Main.class.getClassLoader(), "html")))
                 ).build();
         server.start();
     }
